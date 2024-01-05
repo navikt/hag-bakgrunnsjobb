686a7e700652a41dbe0f7ff2da00ff7365bfcb00
Morten Byhring <morten.byhring@nav.no>
2024-01-05 16:33:26 +0100

    fjerner ktlint

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,12 +2,10 @@ rootProject.name = "hag-bgjobb"
 
 pluginManagement {
     val kotlinVersion: String by settings
-    val kotlinterVersion: String by settings
 
     plugins {
         kotlin("jvm") version kotlinVersion
         kotlin("plugin.serialization") version kotlinVersion
-        id("org.jmailen.kotlinter") version kotlinterVersion
         id("maven-publish")
     }
 }
